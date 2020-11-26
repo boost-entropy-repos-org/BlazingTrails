7a94e764c4cc24d8e0eb43fd2c27f827be837e4e
Glyn Davies <glynwdavies@gmail.com>
2020-11-26 19:40:12 +0000

    Created simulated API.

---
+++ BlazingTrails.Web/Features/Home/Trail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Web.Features.Home
+{
+    public class Trail
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Location { get; set; }
+        public string Time { get; set; }
+        public int Length { get; set; }
+    }
+}
